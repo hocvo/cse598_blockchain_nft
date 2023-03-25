c09ab3910c4bebef8c0c721f7a7a9ef368770f5e
hocvo <chohocvo@gmail.com>
2023-03-25 01:02:21 -0700

    Final

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,17 +8,21 @@ contract MyToken is ERC721URIStorage {
     uint256 private tokenCounter = 0;
     constructor() ERC721("EF1DC563", "B558786E") {}
 
-    // function mint(address to, uint256 tokenId) public returns (uint256)  {
-    //     require(msg.sender == owner);
-    //     _safeMint(to, tokenId);
-    // }
     function mint(address to, string memory tokenURI) public returns (uint256)  {
+        // only owner can mint
         require(msg.sender == owner);
-        // todo get tokenId from uri;
+
+        //generate tokenId and start mint
         uint256 tokenId = tokenCounter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
         tokenCounter = tokenCounter + 1;
+
         return tokenId;
+    }
+    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
+        // update owner when transfer
+        owner = to;
+        safeTransferFrom(from, to, tokenId, "");
     }
 }
